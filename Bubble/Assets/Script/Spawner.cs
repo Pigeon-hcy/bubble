88c1a0fb3aaa9c37feb63197fe382cef8bba6db4
YIMU LIN <yl9772@nyu.edu>
2025-01-25 17:57:51 -0500

    Fuel drop

--- Bubble/Assets/Script/Spawner.cs
+++ Bubble/Assets/Script/Spawner.cs
@@ -4,6 +4,7 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject Coin;
     [SerializeField] private GameObject Star;
+    [SerializeField] private GameObject Fuel;
     public float shootTimer;
     public float shootTime;
     public float starshootTimer;
@@ -34,8 +35,17 @@ public class Spawner : MonoBehaviour
             for (int i = 0; i < Random.Range(1, 4); i++)
             {
                 Vector3 spawnPoint = new Vector3(Random.Range(randomRad, -randomRad), this.transform.position.y, 0);
-                Instantiate(Star, spawnPoint, Quaternion.identity);
-                starshootTimer = starshootTime + Random.Range(0, 600);
+                var j = Random.Range(0, 4);
+                if (j <= 1)
+                {
+                    Instantiate(Fuel, spawnPoint, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(Star, spawnPoint, Quaternion.identity);
+                }
+
+                starshootTimer = starshootTime + Random.Range(0, 300);
             }
 
         }
